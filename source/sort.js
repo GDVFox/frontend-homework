119fadd3734512662f18e9a6784ba1b9ce81c5cb
GDVFox <gaaveer@gmail.com>
2019-02-05 15:19:28 +0300

    [UPD] Spread syntax for word split

--- source/sort.js
+++ source/sort.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const sort = (sentence) => sentence.split(' ')  // разбили на слова
-                                   .map((word) => word.split('') // разбили слово на буквы
+                                   .map((word) => [...word] // разбили слово на буквы
                                                       .sort(Intl.Collator().compare) // отсортили буквы(и 'ё')
                                                       .join('')) // склеили слово назад
                                    .map((word) => word.charAt(0).toUpperCase() // первая буква upper
