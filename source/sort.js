9c1189fa44034fe2b4ff2059dbf47c2607d6da25
GDVFox <gaaveer@gmail.com>
2019-02-06 08:04:28 +0300

    [UPD] Using const for sorted word save

--- source/sort.js
+++ source/sort.js
@@ -1,14 +1,13 @@
 'use strict';
 
-String.prototype.capitalize = function() { 
-    return this.charAt(0).toUpperCase() // первая буква upper
-        + this.slice(1).toLowerCase(); // остальные lower
-    }  
 const sort = (sentence) => sentence.trim() // срезали боковые пробелы
                                     .replace(/\s+/g, " ") // заменили промежутки на 1 пробел
                                     .split(' ')  // разбили на слова
-                                    .map((word) => [...word] // разбили слово на буквы
-                                                        .sort(Intl.Collator().compare) // отсортили буквы(и 'ё')
-                                                        .join('').capitalize()) // склеили слово назад
+                                    .map((word) =>  { 
+                                        const sorted = [...word] // разбили слово на буквы
+                                            .sort(Intl.Collator().compare) // отсортили буквы(и 'ё')
+                                            .join(''); // склеили слово назад
+                                        return sorted.charAt(0).toUpperCase() + sorted.slice(1).toLowerCase();
+                                    })
                                     .sort(Intl.Collator().compare) // отсортили слова(с учётом 'ё')
-                                    .join(' '); // склеиваем новое предложение
+                                    .join(' '); // склеиваем новое предложение                                
